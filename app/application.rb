bdaaa6a600e43e95c62a54a5cf371f84a9026e92
Marianne Pyon <mariannepyon@gmail.com>
2019-10-21 13:50:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,17 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
-    resp.finish
-  end
 
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
+
+    if num1==num_2 && num_2==num_3
+      resp.write "Hello, World"
+    else
+      resp.finish
+    end
+
+    resp.finish
+    end
 end
